42c4d142b9428a2c3995ac5bbc566dd3e3cee7c6
Nicole Guivisdalsky <nituguivi@gmail.com>
2014-06-08 17:53:55 -0300

    save the entered data for each category on local storage

---
+++ client/TuttiFrutti/app/src/main/java/com/example/tuttifrutti/app/Classes/RoundResult.java
@@ -0,0 +1,12 @@
+package com.example.tuttifrutti.app.Classes;
+
+import java.util.Date;
+
+/**
+ * Created by Nituguivi on 07/06/2014.
+ */
+public class RoundResult {
+    public int RoundId;
+    public Date[] CategoriesTimeStamp;
+    public String[] CategoriesValues;
+}
