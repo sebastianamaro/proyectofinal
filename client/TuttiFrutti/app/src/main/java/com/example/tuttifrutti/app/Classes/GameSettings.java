794804cc6811868d7b72e40e772383467dcb8355
Sebastian Amaro <sebastian.amaro@hotmail.com>
2014-06-25 00:15:26 -0300

    Create game refactored to be asynchronus

---
+++ client/TuttiFrutti/app/src/main/java/com/example/tuttifrutti/app/Classes/GameSettings.java
@@ -0,0 +1,28 @@
+package com.example.tuttifrutti.app.Classes;
+
+/**
+ * Created by Sebastian on 24/06/2014.
+ */
+public class GameSettings {
+    private Boolean mode;
+    private Boolean opponents;
+    private Boolean categories;
+
+    public GameSettings(Boolean mode, Boolean opponents, Boolean categories) {
+        this.mode = mode;
+        this.opponents = opponents;
+        this.categories = categories;
+    }
+
+    public Boolean getMode() {
+        return mode;
+    }
+
+    public Boolean getCategories() {
+        return categories;
+    }
+
+    public Boolean getOpponents() {
+        return opponents;
+    }
+}
--- client/TuttiFrutti/app/src/main/java/com/example/tuttifrutti/app/CreateGameActivity.java
+++ client/TuttiFrutti/app/src/main/java/com/example/tuttifrutti/app/CreateGameActivity.java
@@ -3,6 +3,7 @@ package com.example.tuttifrutti.app;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,6 +14,7 @@ import android.widget.Switch;
 import com.example.TuttiFruttiAPI;
 import com.example.tuttifrutti.app.Classes.PlayServicesHelper;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.example.tuttifrutti.app.Classes.GameSettings;
 
 
 public class CreateGameActivity extends ActionBarActivity {
@@ -49,31 +51,57 @@ public class CreateGameActivity extends ActionBarActivity {
         boolean mode = ((Switch)findViewById(R.id.modeSelector)).isChecked(); //true:online, false:offline
         boolean oponents = ((Switch)findViewById(R.id.oponentsSelector)).isChecked(); //true:aleatorio, false:con amigos
         boolean categories = ((Switch)findViewById(R.id.categoriesSelector)).isChecked(); //true:controladas, false:libres
-        TuttiFruttiAPI api= new TuttiFruttiAPI(getString(R.string.server_url));
+        GameSettings gs= new GameSettings(mode,oponents,categories);
+        CreateGameTask task= new CreateGameTask();
+        task.execute(gs);
 
-        PlayServicesHelper helper = new PlayServicesHelper();
-        String regid = "";
-        if (helper.checkPlayServices(this))
-        {
-            regid = helper.getRegistrationId(getApplicationContext());
-            if (regid == "")
-                helper.registerGCMInBackground(getApplicationContext());
-        }
-
-        api.createGame(mode,oponents,categories, regid);
-
-        AlertDialog ad = new AlertDialog.Builder(this).create();
-        ad.setCancelable(false); // This blocks the 'BACK' button
-        ad.setMessage("Se ha creado la partida!");
-        ad.setButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-            }
-        });
-        ad.show();
 
     }
 
+    private class CreateGameTask extends AsyncTask<GameSettings,Void, Void>{
+
+        AlertDialog ad;
+        @Override
+        protected Void doInBackground(GameSettings... settings) {
+
+            GameSettings gs=settings[0];
+            TuttiFruttiAPI api= new TuttiFruttiAPI(getString(R.string.server_url));
+
+            PlayServicesHelper helper = new PlayServicesHelper();
+            String regid = "";
+            if (helper.checkPlayServices(CreateGameActivity.this))
+            {
+                regid = helper.getRegistrationId(getApplicationContext());
+                if (regid == "")
+                    helper.registerGCMInBackground(getApplicationContext());
+            }
+
+
+            api.createGame(gs.getMode(),gs.getOpponents(),gs.getCategories(),regid);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+
+            ad.setCancelable(false); // This blocks the 'BACK' button
+            ad.setMessage("Se ha creado la partida!");
+            ad.setButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+            });
+            ad.show();
+
+        }
+
+        @Override
+        protected void onPreExecute(){
+            ad=new AlertDialog.Builder(CreateGameActivity.this).create();
+
+
+        }
+    }
 }
