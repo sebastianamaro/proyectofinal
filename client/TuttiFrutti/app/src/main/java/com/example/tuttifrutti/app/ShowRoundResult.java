570fcb687520d69aaa7ed3f0825a0e1d9109a7ce
Nicole Guivisdalsky <nituguivi@gmail.com>
2014-07-17 18:14:44 -0300

    show round result in the client (with hardcoded data)

--- client/TuttiFrutti/app/src/main/java/com/example/tuttifrutti/app/ShowRoundResult.java
+++ client/TuttiFrutti/app/src/main/java/com/example/tuttifrutti/app/ShowRoundResult.java
@@ -1,11 +1,16 @@
 package com.example.tuttifrutti.app;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -28,24 +33,113 @@ public class ShowRoundResult extends ActionBarActivity {
         players[3] = "Kevin";
         players[4] = "Nitu";
 
-        ScrollView sv = new ScrollView(this);
-        HorizontalScrollView hsv = new HorizontalScrollView(this);
+        String[] categories= new String[9];
+        categories[0] = "Animales";
+        categories[1] = "Colores";
+        categories[2] = "Marcas de Auto";
+        categories[3] = "Frutas";
+        categories[4] = "Paises";
+        categories[5] = "Nombres de Telo";
+        categories[6] = "Lagos de la Patagonia";
+        categories[7] = "Constelaciones";
+        categories[8] = "Tu vieja";
 
         TableLayout table = (TableLayout)findViewById(R.id.resultsTable);
 
         TableRow row=new TableRow(this.getApplicationContext());
-
         table.setGravity(Gravity.TOP);
 
+        AddHeaderTextView(row, "Categorias");
+        //todo: esto deberia estar adentro de la recorrida de las lines de la primera categoria
         for (String p : players)
         {
-            TextView text=new TextView(this.getApplicationContext());
-
-            text.setText(p);
-            row.addView(text);
+            AddHeaderTextView(row, p);
         }
 
         table.addView(row);
+
+        TableRow totalScoreRow=new TableRow(this.getApplicationContext());
+        TableRow playersRow=new TableRow(this.getApplicationContext());
+
+        for (int i=0;i<categories.length;i++)
+        {
+            row=new TableRow(this.getApplicationContext());
+                AddHeaderTextView(row, categories[i]);
+                //todo: recorrer las lines!!!!
+                //for (int j=0;j<lines.length;j++) {
+                for (int j=0;j<players.length;j++) {
+
+                    //todo: si i==0 llenar la fila de players:
+                    if (i==0)
+                        AddHeaderTextView(playersRow, "Nitu");
+                    //AddTotalTextView(totalScoreRow, line[j].player);
+
+                    //todo: si i==0 llenar la fila de scores:
+                    if (i==0)
+                        AddTotalTextView(totalScoreRow, "180");
+                        //AddTotalTextView(totalScoreRow, line[j].totalScore);
+
+                    AddContentTextView(row, "valor");
+
+                    //todo: AddContentTextView(row, line[j].plays[i]);
+                    //line[playerLine].plays[category]
+                }
+
+            table.addView(row);
+        }
+
+        table.addView(totalScoreRow);
+
+        //todo: agregar la row de scores
+
+
+    }
+
+    private void AddHeaderTextView(TableRow row, String p) {
+        TextView text=new TextView(this.getApplicationContext());
+        text.setText(p);
+        text.setBackgroundResource(R.drawable.cell_shape);
+        text.setPadding(50, 50, 50, 50);
+        text.setTextColor(Color.parseColor("#C96609"));
+        text.setTextSize(15);
+        text.setTypeface(null, Typeface.BOLD);
+
+        row.addView(text);
+    }
+
+    private void AddTotalTextView(TableRow row, String p) {
+        TextView text=new TextView(this.getApplicationContext());
+        text.setText(p);
+        text.setBackgroundResource(R.drawable.cell_shape);
+        text.setPadding(50, 50, 50, 50);
+        text.setTextColor(Color.parseColor("#10B51B"));
+        text.setTextSize(15);
+        text.setTypeface(null, Typeface.BOLD);
+
+        row.addView(text);
+    }
+
+    private void AddContentTextView(TableRow row, String p) {
+        LinearLayout layout = new LinearLayout(this.getApplicationContext());
+        layout.setBackgroundResource(R.drawable.cell_shape);
+
+        TextView text=new TextView(this.getApplicationContext());
+        text.setText(p);
+        text.setPadding(51, 51, 51, 51);
+        text.setTextColor(Color.parseColor("#C96609"));
+        text.setTextSize(15);
+        text.setTypeface(null, Typeface.NORMAL);
+        layout.addView(text);
+
+        TextView score=new TextView(this.getApplicationContext());
+        score.setText("20");
+        text.setPadding(50, 50, 3, 50);
+        score.setTextColor(Color.parseColor("#10B51B"));
+        score.setTextSize(10);
+        score.setTypeface(null, Typeface.NORMAL);
+        layout.addView(score);
+
+        row.addView(layout);
     }
 
 
