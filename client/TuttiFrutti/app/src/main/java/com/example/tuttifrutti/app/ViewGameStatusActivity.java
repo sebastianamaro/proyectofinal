7703ced34fb7ba973e6101eadb0a926da9e04be0
Sebastian Amaro <sebastian.amaro@hotmail.com>
2014-09-19 01:14:51 -0300

    finished refactor test pending

--- client/TuttiFrutti/app/src/main/java/com/example/tuttifrutti/app/ViewGameStatusActivity.java
+++ client/TuttiFrutti/app/src/main/java/com/example/tuttifrutti/app/ViewGameStatusActivity.java
@@ -1,16 +1,20 @@
 package com.example.tuttifrutti.app;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -34,18 +38,12 @@ public class ViewGameStatusActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_game_status);
 
-        // Get ListView object from xml
         listViewGames = (ListView) findViewById(R.id.listGames);
-
-
         new FillListViewAsyncTask().execute();
-
     }
 
     @Override
     public void onBackPressed() {
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
     }
 
     @Override
@@ -68,170 +66,316 @@ public class ViewGameStatusActivity extends ActionBarActivity {
     }
 
     public void createGame(View view) {
-
+        Intent intent = new Intent(getApplicationContext(), CreateGameActivity.class);
+        startActivity(intent);
     }
 
-    public class FillListViewAsyncTask extends AsyncTask<Void, Void, Void>
-    {
+    public class FillListViewAsyncTask extends AsyncTask<Void, Void, Void> {
         TuttiFruttiAPI api;
         ArrayList<UserGame> games;
         ArrayList<FullGame> invitations;
 
-        protected void onPreExecute(){
-            api=new TuttiFruttiAPI(getString(R.string.server_url));
+        protected void onPreExecute() {
+            api = new TuttiFruttiAPI(getString(R.string.server_url));
         }
 
         @Override
         protected Void doInBackground(Void... filePlays) {
             fbId = FacebookHelper.getUserId();
-            games=api.getGames(fbId);
-            invitations=api.getPendingInvitations(fbId);
+            games = api.getGames(fbId);
+            invitations = api.getPendingInvitations(fbId);
             return null;
         }
 
         @Override
         protected void onPostExecute(Void result) {
-            //Todo hacer un adapter y combinar ambas listas en el list view con separadores.
-        }
-    }
-/*
-    public class ViewGameStatusAsyncTaks extends AsyncTask<Void,Void, ArrayList<UserGame>>
-    {
 
 
-        @Override
-        protected void onPostExecute(ArrayList<UserGame> result) {
+            ArrayList<UserGame> activeGames = new ArrayList<UserGame>();
+            ArrayList<UserGame> finishedGames = new ArrayList<UserGame>();
 
+            for (UserGame ug : games)
+                if (ug.getStatus().equals("CLOSED"))
+                    finishedGames.add(ug);
+                else
+                    activeGames.add(ug);
 
+            GamesAdapter ga = new GamesAdapter(getApplicationContext(), activeGames, invitations, finishedGames);
+            listViewGames.setAdapter(ga);
 
-           UserGameAdapter adapter = new UserGameAdapter(getApplicationContext(), android.R.layout.simple_list_item_2,  android.R.id.text1, result);
-
-
-            // Assign adapter to ListView
-            listViewGames.setAdapter(adapter);
-
-            // ListView Item Click Listener
             listViewGames.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view,
                                         int position, long id) {
 
-                    // ListView Clicked item index
-                    int itemPosition     = position;
+                    Object itemValue = (Object) listViewGames.getItemAtPosition(position);
 
-                    // ListView Clicked item value
-                    UserGame  itemValue    = (UserGame) listViewGames.getItemAtPosition(position);
-
-
-
+                    if (itemValue instanceof FullGame) {
+                        Intent intent = new Intent(getApplicationContext(), ManageInvitationActivity.class);
+                        intent.putExtra("gameSettings", (FullGame) itemValue);
+                        startActivity(intent);
+                    } else if (itemValue instanceof UserGame) {
+                        // TODO depende del flujo!
+                    }
                 }
-
             });
         }
     }
 
-    public class ViewPendingInvitationsAsyncTaks extends AsyncTask<Void,Void, ArrayList<FullGame>>
-    {
+    public class GamesAdapter extends BaseAdapter {
 
-        @Override
-        protected void onPostExecute(ArrayList<FullGame> result) {
+        Context context;
+        private ArrayList<Object> games;
+        private int activeGamesSeparatorIndex=0; //ex staredCategoriesSeparatorIndex
+        private int invitationsSeparatorIndex; //ex fixedCategoriesSeparatorIndex
+        private int finishedGamesSeparatorIndex; //ex allCategoriesSeparatorIndex
+        private static final int ITEM_VIEW_TYPE_ACTIVE_GAMES_SEPARATOR = 0;
+        private static final int ITEM_VIEW_TYPE_INVITATIONS_SEPARATOR = 1;
+        private static final int ITEM_VIEW_TYPE_FINISHED_GAMES_SEPARATOR = 2;
+        private static final int ITEM_VIEW_TYPE_EMPTY_SEPARATOR = 3;
+        private static final int ITEM_VIEW_TYPE_FULL_GAME_SEPARATOR = 4;
+        private static final int ITEM_VIEW_TYPE_USER_GAME_SEPARATOR = 5;
+        private static final int ITEM_VIEW_TYPE_FINISHED_USER_GAME_SEPARATOR = 6;
+        private static final int ITEM_VIEW_TYPE_COUNT = 7; // 1,2,3) Headers  4) separador invisible 5) Full Game 6) userGame
+        private static final String activeGamesText="Partidas Activas";
+        private static final String invitationsText="Invitaciones";
+        private static final String finishedGamesText="Todas";
 
+        public GamesAdapter(Context context,ArrayList<UserGame> activeGames, ArrayList<FullGame> invitations, ArrayList<UserGame> finishedGames)
+        {
+            this.context=context;
+            this.games= new ArrayList<Object>();
+            this.games.addAll(activeGames);
+            this.games.addAll(invitations);
+            this.games.addAll(finishedGames);
 
-           GameAdapter adapter = new GameAdapter(getApplicationContext(), android.R.layout.simple_list_item_2, android.R.id.text1, result);
+            this.invitationsSeparatorIndex=activeGames.size() + 2; // Es dos porque tenemos el header y una row transparente para dar el feelling de que son grillas separadas
+            this.finishedGamesSeparatorIndex=activeGames.size() +invitations.size()  + 4;
 
-
-            // Assign adapter to ListView
-            listViewInvitations.setAdapter(adapter);
-
-            // ListView Item Click Listener
-            listViewInvitations.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view,
-                                        int position, long id) {
-
-                    int itemPosition     = position;
-
-                    // ListView Clicked item value
-                    FullGame  itemValue    = (FullGame) listViewInvitations.getItemAtPosition(position);
-
-
-
-
-                    Intent intent = new Intent(getApplicationContext(), ManageInvitationActivity.class);
-                    intent.putExtra("gameSettings", itemValue);
-                    startActivity(intent);
-
-                    // ListView Clicked item index
-
-                }
-
-            });
+            this.games.add(activeGamesSeparatorIndex,activeGamesText);
+            this.games.add(invitationsSeparatorIndex,invitationsText);
+            this.games.add(finishedGamesSeparatorIndex,finishedGamesText);
         }
-    }
-*/
-    private class GameAdapter extends ArrayAdapter<FullGame> {
-
-        private ArrayList<FullGame> gameList;
-
-        public GameAdapter(Context context, int resourceId, int textViewResourceId,
-                               ArrayList<FullGame> categoryList) {
-            super(context, resourceId,textViewResourceId, categoryList);
-            this.gameList = new ArrayList<FullGame>();
-            this.gameList.addAll(categoryList);
-        }
-
 
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public int getItemViewType(int position) {
 
-            if (convertView == null) {
-                convertView = super.getView(position, convertView, parent);
-
-            }
-            Game game = gameList.get(position);
-            ((TextView)convertView.findViewById(android.R.id.text1)).setText(game.getOwner().getName());
-            ((TextView)convertView.findViewById(android.R.id.text2)).setText(game.getMode());
-            ((TextView)convertView.findViewById(android.R.id.text1)).setTextColor(getResources().getColor(R.color.black));
-            ((TextView)convertView.findViewById(android.R.id.text2)).setTextColor(getResources().getColor(R.color.black));
-            return convertView;
-
-        }
-
-    }
-
-    private class UserGameAdapter extends ArrayAdapter<UserGame> {
-
-        private ArrayList<UserGame> userGameList;
-
-        public UserGameAdapter(Context context,int resourceId, int textViewResourceId,
-                           ArrayList<UserGame> categoryList) {
-            super(context, resourceId,textViewResourceId, categoryList);
-            this.userGameList = new ArrayList<UserGame>();
-            this.userGameList.addAll(categoryList);
-        }
+            if(position == activeGamesSeparatorIndex)
+                return ITEM_VIEW_TYPE_ACTIVE_GAMES_SEPARATOR;
 
 
+            if(position == invitationsSeparatorIndex-1)
+                return ITEM_VIEW_TYPE_EMPTY_SEPARATOR;
 
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
+            if(position == invitationsSeparatorIndex)
+                return ITEM_VIEW_TYPE_INVITATIONS_SEPARATOR;
 
-            if (convertView == null) {
-                convertView = super.getView(position, convertView, parent);
+            if(position == finishedGamesSeparatorIndex-1)
+                return ITEM_VIEW_TYPE_EMPTY_SEPARATOR;
 
+            if(position == finishedGamesSeparatorIndex)
+                return ITEM_VIEW_TYPE_FINISHED_GAMES_SEPARATOR;
+
+            if(games.get(position) instanceof FullGame)
+                return ITEM_VIEW_TYPE_FULL_GAME_SEPARATOR;
+
+            if(games.get(position) instanceof UserGame)
+            {
+                UserGame ug= (UserGame) games.get(position);
+                if(ug.getStatus().equals("CLOSED"))
+                    return ITEM_VIEW_TYPE_FINISHED_USER_GAME_SEPARATOR;
+                else
+                    return ITEM_VIEW_TYPE_USER_GAME_SEPARATOR;
             }
 
-            UserGame game = userGameList.get(position);
-            ((TextView)convertView.findViewById(android.R.id.text1)).setText(String.valueOf(game.getGameId()));
-            ((TextView)convertView.findViewById(android.R.id.text2)).setText(game.getStatus());
+            return -1;
+        }
 
-            ((TextView)convertView.findViewById(android.R.id.text1)).setTextColor(getResources().getColor(R.color.black));
-            ((TextView)convertView.findViewById(android.R.id.text2)).setTextColor(getResources().getColor(R.color.black));
+        @Override
+        public int getViewTypeCount() { return ITEM_VIEW_TYPE_COUNT; }
+
+        @Override
+        public int getCount() {
+            return games.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return games.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public boolean isEnabled(int position) {
+            // A separator cannot be clicked !
+            return getItemViewType(position) == ITEM_VIEW_TYPE_FULL_GAME_SEPARATOR || getItemViewType(position) == ITEM_VIEW_TYPE_USER_GAME_SEPARATOR;
+        }
+
+        private class ActiveGamesViewHolder {
+            TextView text1;
+            TextView text2;
+        }
+
+        private class FinishedGamesViewHolder {
+            TextView text1;
+            ImageView imgDelete;
+        }
+
+        private class InvitationsViewHolder {
+            TextView text1;
+            TextView text2;
+        }
+
+
+        @Override
+        public View getView(final int i, View convertView, ViewGroup viewGroup) {
+            final int type = getItemViewType(i);
+
+            switch (type)
+            {
+                case ITEM_VIEW_TYPE_EMPTY_SEPARATOR:
+                    convertView=SetRowEmptyViewHolder(convertView);
+                    break;
+                case ITEM_VIEW_TYPE_ACTIVE_GAMES_SEPARATOR:
+                    convertView=SetRowActiveGamesSeparatorViewHolder(convertView);
+                    break;
+                case ITEM_VIEW_TYPE_INVITATIONS_SEPARATOR:
+                    convertView=SetRowInvitationsSeparatorViewHolder(convertView);
+                    break;
+                case ITEM_VIEW_TYPE_FINISHED_GAMES_SEPARATOR:
+                    convertView=SetRowFinishedSeparatorViewHolder(convertView);
+                    break;
+                case ITEM_VIEW_TYPE_USER_GAME_SEPARATOR:
+                    convertView=SetRowActiveGamesViewHolder(i, convertView);
+                    break;
+                case ITEM_VIEW_TYPE_FULL_GAME_SEPARATOR:
+                    convertView=SetRowInvitationsViewHolder(i,convertView);
+                    break;
+                case ITEM_VIEW_TYPE_FINISHED_USER_GAME_SEPARATOR:
+                    convertView=SetRowFinishedGamesViewHolder(i,convertView);
+                    break;
+
+            }
+
             return convertView;
+
 
         }
 
+        private View SetRowEmptyViewHolder(View convertView) {
+
+            LayoutInflater mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.empty_list_separator, null);
+            }
+            return convertView;
+        }
+
+        private View SetRowActiveGamesSeparatorViewHolder(View convertView) {
+
+            LayoutInflater mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.active_games_list_separator, null);
+            }
+            return convertView;
+        }
+
+        private View SetRowInvitationsSeparatorViewHolder(View convertView) {
+
+            LayoutInflater mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.invitations_list_separator, null);
+            }
+            return convertView;
+        }
+
+        private View SetRowFinishedSeparatorViewHolder(View convertView) {
+
+            LayoutInflater mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.finished_games_separator, null);
+            }
+            return convertView;
+        }
+
+        private View SetRowActiveGamesViewHolder(int position, View convertView) {
+            ActiveGamesViewHolder holder = null;
+
+            LayoutInflater mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.active_game_row, null);
+
+                holder = new ActiveGamesViewHolder();
+                holder.text1 = (TextView) convertView.findViewById(R.id.agtext1);
+                holder.text2 = (TextView) convertView.findViewById(R.id.agtext2);
+                convertView.setTag(holder);
+            }
+            else {
+                holder = (ActiveGamesViewHolder) convertView.getTag();
+            }
+
+            UserGame rowItem = (UserGame) getItem(position);
+
+            holder.text1.setText(rowItem.getGameId());
+            holder.text2.setText(rowItem.getStatus());
+
+            return convertView;
+        }
+
+        private View SetRowInvitationsViewHolder(int position, View convertView) {
+            InvitationsViewHolder holder = null;
+
+            LayoutInflater mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.invitation_row, null);
+
+                holder = new InvitationsViewHolder();
+                holder.text1 = (TextView) convertView.findViewById(R.id.invtext1);
+                holder.text2 = (TextView) convertView.findViewById(R.id.invtext2);
+                convertView.setTag(holder);
+            }
+            else {
+                holder = (InvitationsViewHolder) convertView.getTag();
+            }
+
+            FullGame rowItem = (FullGame) getItem(position);
+
+            holder.text1.setText(rowItem.getOwner().getName());
+            holder.text2.setText(rowItem.getMode());
+
+            return convertView;
+        }
+
+        private View SetRowFinishedGamesViewHolder(int position, View convertView) {
+            FinishedGamesViewHolder holder = null;
+
+            LayoutInflater mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.finished_game_row, null);
+
+                holder = new FinishedGamesViewHolder();
+                holder.text1 = (TextView) convertView.findViewById(R.id.fgtext1);
+                holder.imgDelete = (ImageView) convertView.findViewById(R.id.deleteFinishedGame);
+                convertView.setTag(holder);
+            }
+            else {
+                holder = (FinishedGamesViewHolder) convertView.getTag();
+            }
+
+            FullGame rowItem = (FullGame) getItem(position);
+
+            holder.text1.setText(rowItem.getOwner().getName());
+
+            return convertView;
+        }
+
+
     }
+
 }
